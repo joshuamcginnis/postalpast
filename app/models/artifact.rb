fb8a16b03e0c85d39b89d9a5097bf99ffb7fea08
Josh McGinnis <joshua@mcginnis.io>
2022-08-05 07:26:29 -0700

    Enable accepting nested attributes for stamps and publishers.

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,7 +1,12 @@
 class Artifact < ApplicationRecord
   enum :kind, [:postcard], default: :postcard
+
   has_many :photos, dependent: :destroy
+  has_one :publisher
+  has_one :stamp
+
   accepts_nested_attributes_for :photos, allow_destroy: true
+  accepts_nested_attributes_for :stamp, :publisher
 
   def postmarked?
     postmarked_at.nil? ? false : true
