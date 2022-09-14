3c54a8c653e0375b625776dc657150d5f3223fbc
Josh McGinnis <joshua@mcginnis.io>
2022-09-14 11:49:54 -0700

    Add address to factories.

---
+++ spec/factories/artifact.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :artifact do
+    transient do
+      address do
+        {
+          'street'   => Faker::Address.street_address,
+          'city'     => Faker::Address.city,
+          'state'    => Faker::Address.state_abbr,
+          'postcode' => Faker::Address.postcode
+        }
+      end
+    end
+
+    kind                 { :postcard }
+    addressed_to_name    { Faker::Name.unique.name }
+    addressed_from_name  { Faker::Name.unique.name }
+    addressed_to_message { Faker::Quote.yoda }
+    color                { true }
+    subject              { Faker::Book.title }
+
+    # addresses
+    subject_address      { address }
+    postmark_address     { address }
+    to_address           { address }
+    from_address         { address }
+
+    trait :with_photos do
+      photos do
+        [create(:photo, face: :front), create(:photo, face: :back)]
+      end
+    end
+  end
+end
